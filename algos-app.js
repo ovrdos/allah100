92b91381a260ce2fdbc76396ef8844e178486792
Hakim <ovrdos@users.noreply.github.com>
2018-01-09 15:50:40 -0700

    Update algos-app.js

--- algos-app.js
+++ algos-app.js
@@ -28,7 +28,7 @@ var displayAttributes = function(index) {
 }
 
 var setBackground = function() {
-  var max = 1765;
+  var max = 43;
   var min = 0;
   var idx = Math.random() * (max - min) + min;
   document.body.style.backgroundImage='url('+nature[Math.round(idx)]+')';
